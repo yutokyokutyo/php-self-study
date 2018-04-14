3db9c9d01954de2a7bbb18f12ac6dbbf18c1f62b
y.kondo <k.yuto.cre@gmail.com>
2018-04-14 12:29:59 +0900

    p67

---
+++ demension.php
@@ -0,0 +1,20 @@
+<?php
+$data = [
+    [
+        'name' => 'とうゆうどんこ',
+        'age' => 28,
+        'sex' => 'man',
+    ],
+    [
+        'name' => 'こげる',
+        'age' => 28,
+        'sex' => 'man',
+    ],
+    [
+        'name' => 'かなちゃん',
+        'age' => 29,
+        'sex' => 'woman',
+    ],
+];
+
+print_r($data);
