2c7a83c0db107ec8f79e1c934550466b4e8e6f67
y.kondo <k.yuto.cre@gmail.com>
2018-04-14 15:08:41 +0900

    p222 require_once

--- chapter6/include.php
+++ chapter6/include.php
@@ -3,6 +3,3 @@
 function getTriangleArea(float $base, float $height): float {
     return $base * $height / 2;
 }
-
-$area = getTriangleArea(8, 10);
-print "三角形の面積は {$area} です！";
---
+++ chapter6/require_once.php
@@ -0,0 +1,5 @@
+<?php
+require_once './include.php';
+
+$area = getTriangleArea(8, 10);
+print "三角形の面積は {$area} です！";
